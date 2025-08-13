185d6390ce64f9bc53b4ef8542c84f10a4edef65
madhumitha3147 <pamminajahnavi@gmail.com>
2025-08-13 06:16:06 +0530

    Add root URL handler to fix 404 on /

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -17,12 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse  # <-- Add this import
+
+def root_view(request):
+    return HttpResponse("Backend API is running")
 
 urlpatterns = [
-     path('', root_view), 
+    path('', root_view), 
     path("admin/", admin.site.urls),
     path("api/", include("logistics.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
 
